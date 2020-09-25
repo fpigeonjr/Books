6fbd90d6c8596838217f878b6358d736acc78ccb
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-09-24 19:20:07 -0500

    adds dropdown to styleguide

---
+++ css-in-depth/ch10/js/docs.js
@@ -0,0 +1,9 @@
+(function () {
+const dropdowns = document.querySelectorAll('.dropdown__toggle')
+Array.prototype.forEach.call(dropdowns, (dropdown) => {
+  dropdown.addEventListener('click', (e) => {
+    e.target.parentNode.classList.toggle('is-open')
+  })
+})
+
+}())
